81a0fbad2b84eb17a2d2ecf166f4185e94fa1155
Rachmanto Saputra <rachmantosaputra@outlook.jp>
2020-07-14 18:21:06 +0700

    Initial Commit

---
+++ aia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aia
+//
+//  Created by Rachmanto Saputra on 14/07/20.
+//  Copyright © 2020 Rachmanto Saputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
