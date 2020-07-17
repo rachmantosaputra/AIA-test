fda13fb11667df7eccb4dd53c630473a0e2563d5
Rachmanto Saputra <rachmantosaputra@outlook.jp>
2020-07-17 11:29:13 +0700

    first commit

---
+++ Swift/Date.swift
@@ -0,0 +1,9 @@
+//
+//  Date.swift
+//  aia
+//
+//  Created by Rachmanto Saputra on 17/07/20.
+//  Copyright © 2020 Rachmanto Saputra. All rights reserved.
+//
+
+import Foundation
---
+++ Swift/Value.swift
@@ -0,0 +1,9 @@
+//
+//  Value.swift
+//  aia
+//
+//  Created by Rachmanto Saputra on 17/07/20.
+//  Copyright © 2020 Rachmanto Saputra. All rights reserved.
+//
+
+import Foundation
